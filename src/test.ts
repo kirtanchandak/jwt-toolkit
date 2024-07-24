ef1361cc00f1d233381e181fe90e69121fb1fa66
kirtanchandak <themarketingkirtan@gmail.com>
2024-07-24 13:25:25 +0530

    strategy-change

--- src/test.ts
+++ src/test.ts
@@ -1,22 +1,8 @@
-import { encode_jwt, decode_jwt, validate_jwt } from "./jwt";
+import { encode_jwt} from "./jwt";
 
 const secret = 'secret';
 const payload = { userId: 123, name: 'Anish' };
 
 // Encode
-const token = encode_jwt(secret, 1, payload, 3000);
+const token = encode_jwt(secret, 1, payload);
 console.log('Encoded Token:', token);
-
-//decode
-const decoded = decode_jwt(secret, token);
-console.log('Decoded Token:', decoded);
-
-// Validate
-const isValid = validate_jwt(secret, token);
-console.log('Is Token Valid:', isValid);
-
-// Test expiration
-setTimeout(() => {
-  const isExpired = validate_jwt(secret, token);
-  console.log('Is Token Valid after 2 seconds:', isExpired);
-}, 2000);
