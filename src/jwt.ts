4a415c1d571c14da539d76baf50cac13646b923b
kirtanchandak <themarketingkirtan@gmail.com>
2024-07-24 21:54:37 +0530

    change

--- src/jwt.ts
+++ src/jwt.ts
@@ -2,10 +2,10 @@ let subtleCrypto: SubtleCrypto;
 
 if (typeof window !== 'undefined' && window.crypto) {
   // Browser environment
-  subtleCrypto = window.crypto.subtle;
+  subtleCrypto = crypto.subtle;
 } else if (typeof global !== 'undefined' && (global as any).crypto) {
   // Node.js environment
-  subtleCrypto = (global as any).crypto.webcrypto.subtle;
+  subtleCrypto = (global as any).crypto.subtle;
 } else {
   throw new Error('No Web Crypto API available.');
 }
